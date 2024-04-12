e68d8813fc056a70beba986114d843af0eeb0d50
Prasun Gupta <prasungupta604@gmail.com>
2024-04-12 14:09:13 +0530

    create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between iteam-centre max-w-6xl mx-auto">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Aao</span>
-          <span className="text-slate-700">Padharo</span>
-        </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Aao</span>
+            <span className="text-slate-700">Padharo</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -19,15 +21,19 @@ export default function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4 items-center">
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Home
-          </li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Sign in
-          </li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className=" text-slate-700 hover:underline">Sign in</li>
+          </Link>
         </ul>
       </div>
     </header>
